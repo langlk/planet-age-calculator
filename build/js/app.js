e09013e592a20233dc4a18921f2f13bea474ef3e
Kelsey Langlois <kels.langlois@gmail.com>
2017-10-13 08:32:10 -0700

    Add initial project files.

---
+++ build/js/app.js
@@ -0,0 +1,34 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var AgeCalculator = exports.AgeCalculator = function AgeCalculator() {
+  _classCallCheck(this, AgeCalculator);
+};
+
+},{}],2:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var PlanetYearConverter = exports.PlanetYearConverter = function PlanetYearConverter() {
+  _classCallCheck(this, PlanetYearConverter);
+};
+
+},{}],3:[function(require,module,exports){
+'use strict';
+
+var _ageCalculator = require('./../js/age-calculator.js');
+
+var _planetYearConverter = require('./../js/planet-year-converter.js');
+
+},{"./../js/age-calculator.js":1,"./../js/planet-year-converter.js":2}]},{},[3]);
---
+++ js/planet-age-interface.js
@@ -0,0 +1,2 @@
+import { AgeCalculator } from './../js/age-calculator.js'
+import { PlanetYearConverter } from './../js/planet-year-converter.js'
